665880bbe996a68fb07ee819361a86f55e364637
Sujit Baniya <itsursujit@gmail.com>
2023-01-01 10:28:55 +0545

    Add meta tag

--- src/main.tsx
+++ src/main.tsx
@@ -8,15 +8,21 @@ import 'uno.css'
 const AuthProvider = lazy(() => import("~/core/hooks/auth/AuthProvider"));
 const Routes = lazy(() => import("~/routes"));
 import reportWebVitals from  './reportWebVitals'
+import {Helmet, HelmetProvider} from "~/packages/helmet";
 
 const container = document.getElementById('root')
 const root = createRoot(container!)
 root.render(
     <StrictMode>
         <HashRouter>
-            <AuthProvider>
-                <Routes/>
-            </AuthProvider>
+            <HelmetProvider>
+                <Helmet>
+                    <title>Test App</title>
+                </Helmet>
+                <AuthProvider>
+                    <Routes/>
+                </AuthProvider>
+            </HelmetProvider>
         </HashRouter>
     </StrictMode>
 )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,1 +1,12 @@
-export const Home = () => <h1 className="text-red-400">I'm home</h1>
+import {Helmet} from "~/packages/helmet";
+
+export const Home = () => {
+    return (
+        <>
+            <Helmet>
+                <title>Home</title>
+            </Helmet>
+            <h1 className="text-red-400">I'm home</h1>
+        </>
+    )
+}
